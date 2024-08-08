7cf2e5fae616d5847b440f6d7d632895629f3020
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-08-07 21:48:25 -0300

    Update prova.ts

--- prova.ts
+++ prova.ts
@@ -1,1 +1,24 @@
+let reader = require("readline-sync")
 
+interface Prova {
+    constructor(gabarito: Array<string>);
+    respostaAluno(resposta: string): void;
+    acertos(): number;
+    nota(): number;
+    maior(outraProva: Prova): number;
+}
+
+class Prova implements Prova {
+    gabarito: Array<string>
+    respostas: string[]= []
+
+    constructor(gabarito: string[]) {
+        this.gabarito = ['A', 'B', 'E', 'D', 'B', 'C', 'C', 'A', 'B', 'E', 'D', 'D', 'B', 'C', 'C']
+    }
+
+    respostaAluno(resposta: string): void {
+        
+    }
+    setProva(){
+    }
+}
