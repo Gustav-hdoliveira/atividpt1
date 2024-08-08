c72fa648f57c618bf518cf25cc3edcf3c0d488a4
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-08-07 21:47:01 -0300

    Update voo.ts

--- voo.ts
+++ voo.ts
@@ -2,25 +2,22 @@ let reader = require('readline-sync')
 //Parte 3
 import { Data } from "./atividadPT1";
 class Voo {
-    numeroVoo: number
+    numeroVoo: string
     data: Data
     acento: Array<number>
-    constructor(numeroVoo: number, data: Data){
+    constructor(numeroVoo: string, data: Data){
         this.numeroVoo = numeroVoo
+        this.data = data
         this.acento = []
     }
-    setvoo(){
-        let vooup = reader.questionInt('qual o numero do proximo voo disponivel? ')
-        this.numeroVoo=vooup
-    }
     proximoLivre(){
-        console.log(this.numeroVoo[0], this.data[0])
+        console.log(this.numeroVoo[0])
     }
     verifica(cadeira: number ): void{
         for (let A = 0; A < 101; A++) {
             this.acento.push(0) // adiciona valores todas as vezes que o ciclo é começado
             let element = this.acento[cadeira] //seleciona qual o valor dentro do array que vai ser celecionado
-            if (A = 101) {
+            if (A = 100) {
                 if (element == 0) {
                     console.log(`A cadeira nº ${cadeira} está ocupada`)    
                     console.log(element)
@@ -28,23 +25,53 @@ class Voo {
                     console.log(`A cadeira nº ${cadeira} está livre`)
                     console.log(element)
                 }
-            } else {
-
             }
         }
         
     }
     ocupa(cadeira: number): void{
         this.acento[cadeira + 1] = 1
-        if (this.acento[cadeira] >= 1) {
-            console.log("Parabens sua reserva foi realizada")
-        } else {
+        if (this.acento[cadeira] == 0) {
             console.log("Tente novamente")
+        } else {
+            console.log("Parabens sua reserva foi realizada")
         }
     }
-
+    vagas():void{
+        for (let w = 0; w < this.acento.length; w++) {
+            
+            if (this.acento[w] < 1) {
+                console.log(`o acento nº:${this.acento[w] + 1} está livre.`)
+            }
+        }
+    }
+    setVOO(): void{
+        let numvooUP = reader.question("Qual o numero do próximo voo disponivel? ")
+        this.numeroVoo = numvooUP
+        let dayup = reader.question("Digite um dia: ")
+        let mouthup = reader.question("DIgite um mes: ")
+        let anoup = reader.question("Digite um ano: ")
+        this.data.dia = dayup
+        this.data.mes = mouthup
+        this.data.ano = anoup
+    }
+    getvoo(): string{
+        return this.numeroVoo
+    }
+    getData(){
+        return this.data
+    }
+    clone(): Voo{
+        return new Voo(this.numeroVoo, this.data)
+    }
 }
 let dataNovo = new Data(10, 11, 2000)
-const voonovo = new Voo(20, dataNovo)
-voonovo.verifica(30)
-voonovo.ocupa(30)
+const voonovo = new Voo("20B", dataNovo)
+
+voonovo.setVOO
+console.log(voonovo.verifica(30))
+console.log(voonovo.ocupa(30))
+console.log(voonovo.proximoLivre())
+console.log(voonovo.vagas())
+
+console.log(voonovo.acento)
